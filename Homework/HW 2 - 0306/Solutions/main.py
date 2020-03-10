ed78ea55f74c5344569cb62d4c76b691f3f5b521
Luca Medeiros <lucamedeiros@outlook.com>
2020-03-10 20:17:02 +0900

    Upload solution HW1-2

---
+++ Homework/HW 2 - 0306/Solutions/main.py
@@ -0,0 +1,22 @@
+import cv2
+ 
+# Read image
+img = cv2.imread('cat.jpg')
+ 
+# Get dimensions of image
+dimensions = img.shape
+ 
+# Height and width in image
+height = img.shape[0]
+width = img.shape[1]
+
+# Find center of the image
+center_H = int(height/2)
+center_W = int(width/2)
+print(center_H, center_W)
+
+# Crop 500x500 from the center
+cropped = img[center_H-250:center_H+250, center_W-250:center_W+250]
+
+# Save a new image file
+cv2.imwrite("crop.jpg", cropped)
