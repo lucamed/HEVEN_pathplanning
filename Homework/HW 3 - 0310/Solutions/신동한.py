7e1117a1b8f440c470bdc8762edeea507b19cc37
donghan-Sin <56184818+donghan-Sin@users.noreply.github.com>
2020-03-14 18:56:45 +0900

    Update 신동한.py

--- Homework/HW 3 - 0310/Solutions/신동한.py
+++ Homework/HW 3 - 0310/Solutions/신동한.py
@@ -8,16 +8,16 @@ image = cv2.imread("sign.jpg")
 sign = image[80:190, 20:130]
 cv2.imshow("sign",sign)
 cv2.waitKey(0)
-
+road = cv2.imread("road.jpg")
 for i in range(100):
-    road = cv2.imread("road.jpg")
     road_H = road.shape[0]
     road_W = road.shape[1]
     a = random.randrange(0, road_H-110)
     b = random.randrange(0, road_W-110)
     road_copy = road.copy()
     road_copy[a:a+110,b:b+110] = sign
-    cv2.imwrite('road00{}.jpg'.format(i))
+    cv2.imwrite('road00{}.jpg'.format(i), road_copy)
+
 
 
 
