02994f7bba327a4824edd1a8dada1caab6bf4fcc
Luca <lucamedeiros@outlook.com>
2020-04-13 21:59:14 +0900

    -s

--- Homework/HW 2 - 0306/Solutions/오혜준.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-img=Image.open('C:\\Users\\USER\\Downloads\\hw_0306\\hw_week1\\hw_2\\cat.jpg')
-print(img.size)
-imgcrop = img.crop((550, 283, 1050, 783))
-imgcrop.show()
-imgcrop.save('catcrop.jpg')
