cd340de9830a10b7d8a45dc0d7a2b2fa7a6edbdf
sjy268 <62732827+sjy268@users.noreply.github.com>
2020-04-15 13:34:57 +0900

    Add files via upload

---
+++ Homework/HW 2 - 0306/Solutions/김민창.py
@@ -0,0 +1,16 @@
+import cv2
+
+photo=cv2.imread("cat.jpg",cv2.IMREAD_COLOR)
+
+[x,y,z]=photo.shape
+i=x//2;
+j=y//2;
+
+crop = photo.copy()
+crop = photo[i-250:i+250 , i-250:i+250]
+
+cv2.imshow("photo",photo)
+cv2.imshow("crop", crop)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
