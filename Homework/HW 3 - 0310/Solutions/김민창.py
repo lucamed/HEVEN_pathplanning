80907bf934211f739163f9ddfa17a4deb2cc6c16
sjy268 <62732827+sjy268@users.noreply.github.com>
2020-04-22 00:32:06 +0900

    김민창 HW3

--- Homework/HW 3 - 0310/Solutions/김민창.py
+++ Homework/HW 3 - 0310/Solutions/김민창.py
@@ -12,6 +12,7 @@ road = Image.open("road.jpg")
 area = (130,90,250,200)  # set the area for getting sign
 cropped = sign.crop(area)  # crop the one sign from a lot of signs
 
+
 for i in range(1,101):
     x=random.randint(0,1579)  # get a random number for x,y coordinates and these number is obtained by calculating
     y=random.randint(0,1021)  # original size of road images(1696*1131) and sign(120*110)
